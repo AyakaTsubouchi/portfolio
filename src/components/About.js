e9b60027b485ef6e5453995f7e6c381314706f20
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-04-26 00:08:20 -0700

    add style on about and works

--- src/components/About.js
+++ src/components/About.js
@@ -4,19 +4,41 @@ import { Consumer } from './Context';
 const jpnese =
   '初めまして,坪内文香と申します。フロントエンドディベロッパーです。大学卒業後、一般企業で経理&webサイト担当として合計６年間働いていましたが、30歳で一念発起しバンクーバーへ留学。そこでプログラミングを学び今に至ります。メンテナンスのしやすいサイト作りを心がけます。';
 const eng =
-  "Hi, I ' m Yuria Nishimura.A Frontend Developer familiar with UI/UX.My specialty is solving problems through rational clear UI design from the view of users, and frontend development writing readable clean codes. The infinite potential and complex diversity of web technologies jerked my curiosity to know overwhelming ideas and technologies when I learned how to make a website.";
+  "This is other programmer's introduction. hahaha. Hi, I ' m  Ayaka Tsubouchi.A Frontend Developer familiar with UI/UX.My specialty is solving problems through rational clear UI design from the view of users, and frontend development writing readable clean codes. The infinite potential and complex diversity of web technologies jerked my curiosity to know overwhelming ideas and technologies when I learned how to make a website.";
 
 const About = () => {
   return (
     <Consumer>
       {({ isEnglish }) => (
-        <div className="ui container">
-          <div className="about">
-            <h2>About</h2>
+        <div className="first-section section">
+          <div className="ui container about">
+            <h2>
+              <span>About</span>
+            </h2>
             <p>Web Deisgner / Developer</p>
             {isEnglish}
-            {isEnglish ? <p>{eng}</p> : <p>{jpnese}</p>}
-            <h2>Skils</h2>
+            {isEnglish ? (
+              <p className="text">{eng}</p>
+            ) : (
+              <p className="text">{jpnese}</p>
+            )}
+            <div className="about first-section">
+              <h2>
+                <span>Skils</span>
+              </h2>
+              <div className="section">
+                <h3 className="section-title">languages</h3>
+                <p>Mainly writing TypeScript for React with VS Code.</p>
+                <ul>
+                  <li>HTML & CSS</li>
+                  <li>SCSS</li>
+                  <li>JavaScript</li>
+                  <li>GitHub</li>
+                  <li>Java:little bit</li>
+                  <li>Python:little bit</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       )}
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,10 +1,30 @@
 import React, { useContext } from 'react';
 import { Consumer } from './Context';
+import fourcastImg from './images/fourcast.png';
 
 export default function Works() {
   return (
     <Consumer>
-      {({ isEnglish }) => <div className="works ui container"></div>}
+      {({ isEnglish }) => (
+        <div className="first-section section">
+          <div className="ui container about">
+            <h2>
+              <span>Works</span>
+            </h2>
+            <p>Web Deisgner / Developer</p>
+            <div className="first-section">
+              <h3 className="section-title">Four cast</h3>
+              <img src={fourcastImg}></img>
+              <p>This is e commerce website.</p>
+            </div>
+            <div className="first-section">
+              <h3 className="section-title">Four cast</h3>
+              <img src={fourcastImg}></img>
+              <p>This is e commerce website.</p>
+            </div>
+          </div>
+        </div>
+      )}
     </Consumer>
   );
 }
