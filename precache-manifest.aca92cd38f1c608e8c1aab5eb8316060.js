25e322afcb0cf078eab19bd18f282cdac3eac7b4
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-05-25 17:24:46 -0700

    Updates

---
+++ precache-manifest.aca92cd38f1c608e8c1aab5eb8316060.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f694195770922ee2641abd77719fe9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2fab6c9e12448ba6ad07",
+    "url": "/portfolio/static/css/main.ef174f69.chunk.css"
+  },
+  {
+    "revision": "32ddd8d813215d2dd92f",
+    "url": "/portfolio/static/js/2.6ac03e3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.6ac03e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fab6c9e12448ba6ad07",
+    "url": "/portfolio/static/js/main.8e57f7af.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "8e8efaa8308938309489a6c9d04ff653",
+    "url": "/portfolio/static/media/fourcast.8e8efaa8.png"
+  },
+  {
+    "revision": "59b4a29426090314f372f6a494f1af49",
+    "url": "/portfolio/static/media/linkedIn.59b4a294.jpg"
+  },
+  {
+    "revision": "df776ca640d17de2ecc9529772c60dce",
+    "url": "/portfolio/static/media/weatherApp.df776ca6.png"
+  }
+]);
